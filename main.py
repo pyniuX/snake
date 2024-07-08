2412492665d76be081067943251a020a5f37704c
pyniu <krzysiek01pk@gmail.com>
2024-07-09 00:00:17 +0200

    borders added, snake movement wip

--- main.py
+++ main.py
@@ -3,22 +3,46 @@ from ursina import *
 
 app = Ursina()
 
-arena = Entity(model="cube", color=color.green, scale=(2,1,3))
-snake = Entity(model='cube', color=color.red, scale_y=1, collider='box')
+window.color=color.gray
+# window.fullscreen=True
+camera.orthographic=True
+camera.fov=10
+
+floor = Entity(model='quad', y=-5.5, collider='box', scale=(20,1), visible=True)
+ceiling = duplicate(floor, y=5.5, rotation_z=180, visible=True,)
+left_wall = duplicate(floor, x=-8.5, rotation_z=90, visible=True)
+right_wall = duplicate(floor, x=8.5,  rotation_z=-90, visible=True)
+left_wall.y +=1
+right_wall.y +=1
+
+collison_cooldown = .15
+rotation_cooldown = .25
+snake = Entity(model='cube', scale=0.5, collider='box', color=color.red, collison_cooldown=collison_cooldown, rotation_cooldown=rotation_cooldown)
+
+
 # point = Entity(model='sphere', color=color.red, scale=0.3)
 
-speed_x=speed_y=0.1
+
 
 def update():
-    snake.x += held_keys['d'] * time.dt
-    snake.x -= held_keys['a'] * time.dt
-    snake.y += held_keys['w'] * time.dt
-    snake.y -= held_keys['s'] * time.dt
+    snake.position += snake.up * time.dt * 2
+    snake.rotation_cooldown -= time.dt
 
+    if held_keys['w'] and snake.rotation_cooldown <= 0:
+        snake.look_at_xy(ceiling) 
+        snake.rotation_cooldown = .25
 
-# def input(key):
-    # if key == "space":
-        # player.y += 1
-        # invoke(setattr, player, 'y', player.y-1, delay=.25)
+    if held_keys['s'] and snake.rotation_cooldown <= 0:
+        snake.look_at_xy(floor) 
+        snake.rotation_cooldown = .25
+
+    if held_keys['a'] and snake.rotation_cooldown <= 0:
+        snake.rotation_z -= 90 
+        snake.rotation_cooldown = .25
+
+    if held_keys['d'] and snake.rotation_cooldown <= 0:
+        snake.rotation_z -= 90 
+        snake.rotation_cooldown = .25
+
 
 app.run()
